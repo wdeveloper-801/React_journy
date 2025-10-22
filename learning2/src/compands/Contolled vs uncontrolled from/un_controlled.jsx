d2658ddb7186eabbd632583a0bbcab7bc0903b9d
wdeveloper-801 <wdeveloper149@gmail.com>
2025-10-22 14:50:12 +0500

    Update un_controlled.jsx

--- learning2/src/compands/Contolled vs uncontrolled from/un_controlled.jsx
+++ learning2/src/compands/Contolled vs uncontrolled from/un_controlled.jsx
@@ -20,7 +20,7 @@ let handlePasswordChange = (e) => {
   }
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="bg-blue-500 w-1/2 flex items-center justify-center flex-col rounded-2xl gap-4 mb-20 h-auto">
+      <div className="bg-blue-500 w-1/2 flex items-center justify-center flex-col rounded-2xl gap-4 pb-10 h-auto">
         <h1 className="text-2xl font-bold">Uncontrolled Component</h1>
         <form className="flex flex-col" onSubmit={handleSubmit}>
           <input
