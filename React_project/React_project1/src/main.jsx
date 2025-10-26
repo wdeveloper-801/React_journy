49a2f7e32364812a9f1102acefe5e9d64e668e00
Muhammad saeed <wdeveloper149@gmail.com>
2025-10-26 14:34:24 +0500

    Updated components and dependencies

--- React_project/React_project1/src/main.jsx
+++ React_project/React_project1/src/main.jsx
@@ -3,10 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
-import Home from './compends/Home.jsx'
-import The_brand from './compends/The brand.jsx'
-import Card3 from './compends/Card3.jsx'
-import Card_details_of_1_Card from './compends/Carddetails_of1Card.jsx'
+import Home from './compends/Home/Home.jsx'
+import The_brand from './compends/The Brand/The brand.jsx'
+import Card3 from './compends/Home/Card3.jsx'
+import Card_details_of_1_Card from './compends/Home/Carddetails_of1Card.jsx'
 
 const router = createBrowserRouter(
     createRoutesFromElements(
