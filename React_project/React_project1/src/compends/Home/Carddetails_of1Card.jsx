627154c4165ae2e68359d7204e8cc17cda4c4db8
Muhammad saeed <wdeveloper149@gmail.com>
2025-10-26 17:40:24 +0500

    Updated components and dependencies

--- React_project/React_project1/src/compends/Home/Carddetails_of1Card.jsx
+++ React_project/React_project1/src/compends/Home/Carddetails_of1Card.jsx
@@ -6,12 +6,10 @@ const Card_details_of_1_Card = () => {
     const navigate = useNavigate()
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 ">
-            <img src={state.url} alt={state.name} className="w-[600px] h-[400px] object-cover rounded-lg shadow-lg " />
-            <h1 className="text-3xl font-bold mt-4">{state.name}</h1>
-            <h1 className="text-3xl font-bold mt-4">{state.price}</h1>
-            <button onClick={() => navigate('/')} className="mt-6 bg-purple-600 text-white px-4 py-2 rounded hover:bg-purple-700"> Back </button>
-        </div>
+       <div className='w-full h-screen flex justify-center items-center bg-green-300'>
+        <div className='w-11/12 h-full bg-blue-400'></div>
+        <div className='w-full h-full bg-purple-500'></div>
+       </div>
     )
 }
 
